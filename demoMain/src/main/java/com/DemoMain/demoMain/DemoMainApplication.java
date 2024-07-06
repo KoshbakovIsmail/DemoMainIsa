7d9161a53a3cf237f44d4f8b441faf45d37a0928
KosbakovIsmail <kosbakovismail@gmail.com>
2024-07-06 14:15:02 +0300

    initial commit alex

--- demoMain/src/main/java/com/DemoMain/demoMain/DemoMainApplication.java
+++ demoMain/src/main/java/com/DemoMain/demoMain/DemoMainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoMainApplication {
 
+	//commit Alex
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMainApplication.class, args);
 	}
