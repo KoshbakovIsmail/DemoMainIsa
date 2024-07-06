822655f9cb23811012ac49a6a82f537c7d6eaf98
KosbakovIsmail <kosbakovismail@gmail.com>
2024-07-06 14:07:21 +0300

    initial commit

---
+++ demoMain/src/test/java/com/DemoMain/demoMain/DemoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoMain.demoMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
